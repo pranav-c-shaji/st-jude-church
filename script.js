69b87866eb2c7a0e6f3a41d91f7c956103313494
pranav-c-shaji <166815360+pranav-c-shaji@users.noreply.github.com>
2025-09-09 16:57:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+});
+
+
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    // Change image every 5 seconds (5000 milliseconds)
+    setTimeout(showSlides, 8000);
+}
+
+
+
+
